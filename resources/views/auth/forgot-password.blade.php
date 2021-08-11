f202422f6bdf44eec038f7d310c3b954e6789f75
Alex Batts <alex.batts05@gmail.com>
2021-08-11 00:52:25 -0400

    Refactored forgot password view to new component

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,56 +1,38 @@
 <x-guest-layout title="Forgot Password">
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-          <div class="logoimage">
-          </div>
-        </x-slot>
-        <div class="mt-1"></div>
-        <div>
-
-            <span class="mdc-typography logintext mb-4">
-              Forgot password?
-            </span>
-            <p class="mdc-typography mb-4" style="font-size: 15px;">
-              Enter your email and we'll send you a magic link to reset it!
-            </p>
-        </div>
-
-        @if (session('status'))
-            <div class="mb-2 mdc-typography mdc-typography--body2 text-green">
-                {{ session('status') }}
-            </div>
+  <div class="pt-32 pb-24 roboto">
+    <x-ui.auth-card title="Forgot your password?" description="Enter your email address and we'll send you a magic link to reset it!">
+      <div class="mb-2 text-sm text-green h-4 mt-3">
+        @if (session()->has('status'))
+          {{ session('status') }}
         @endif
-
-
-
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <div class="block">
-              <label class="mdc-text-field mdc-text-field--outlined login-form resemail mt-4">
-                <input type="email" class="mdc-text-field__input" aria-labelledby="email-label" name="email" value="{{ old('email') }}" required autofocus>
-                <span class="mdc-notched-outline">
-                  <span class="mdc-notched-outline__leading"></span>
-                  <span class="mdc-notched-outline__notch">
-                    <span class="mdc-floating-label" id="email-label">Email address</span>
-                  </span>
-                  <span class="mdc-notched-outline__trailing"></span>
+      </div>
+      <form method="POST" action="{{ route('password.email') }}">
+          @csrf
+          <div class="block">
+            <label class="mdc-text-field mdc-text-field--outlined w-full mt-4">
+              <input type="email" class="mdc-text-field__input" aria-labelledby="email-label" name="email" value="{{ old('email') }}" required autofocus>
+              <span class="mdc-notched-outline">
+                <span class="mdc-notched-outline__leading"></span>
+                <span class="mdc-notched-outline__notch">
+                  <span class="mdc-floating-label" id="email-label">Email address</span>
                 </span>
-              </label>
-              @if ($errors->has('email'))
-                @foreach ($errors->get('email') as $message)
-                <div class="mdc-text-field-helper-line">
-                  <div class="mdc-text-field-helper-text mdc-text-field-helper-text--persistent" id="my-helper-id" aria-hidden="true"><p class="text-error">{{ $message }}</p></div>
-                @endforeach
-              @endif
-            </div>
+                <span class="mdc-notched-outline__trailing"></span>
+              </span>
+            </label>
+            @if ($errors->has('email'))
+              @foreach ($errors->get('email') as $message)
+              <div class="mdc-text-field-helper-line">
+                <div class="mdc-text-field-helper-text mdc-text-field-helper-text--persistent" id="my-helper-id" aria-hidden="true"><p class="text-error">{{ $message }}</p></div>
+              @endforeach
+            @endif
+          </div>
 
-            <div class="flex items-center justify-end mt-8">
-                <button class="mdc-button mdc-button-ripple mdc-button--raised">
-                    <span class="mdc-button__ripple"></span>
-                    {{ __('Email Password Reset Link') }}
-                </button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
+          <div class="flex items-center justify-end mt-12">
+            <button class="mdc-button mdc-button-ripple mdc-button--raised">
+              <span class="mdc-button__ripple"></span>Email Password Reset Link
+            </button>
+          </div>
+      </form>
+    </x-ui.auth-card>
+  </div>
 </x-guest-layout>
