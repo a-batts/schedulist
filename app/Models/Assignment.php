c93838cc7ab42a0955fe2bd3c5bd6f317ec29988
Alex Batts <alex.batts05@gmail.com>
2021-09-24 00:04:18 -0400

    Fixed class name not displaying properly

--- app/Models/Assignment.php
+++ app/Models/Assignment.php
@@ -7,57 +7,50 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Classes;
 use Carbon\Carbon;
 
-class Assignment extends Model
-{
-    use HasFactory;
+class Assignment extends Model {
+  use HasFactory;
 
-    protected $table = 'assignments';
+  protected $table = 'assignments';
 
-    protected $guarded = ['id', 'userid', 'classid'];
+  protected $guarded = ['id', 'userid', 'classid'];
 
-    public function getClassNameAttribute($value){
-      if ($this->classid)
-        return 'No Class';
+  public function getClassNameAttribute($value) {
+    if ($this->classid == null)
+      return 'No Class';
 
-      $class = Classes::where('id', $this->classid)->first();
-      if ($class != null)
-        return 'Deleted Class';
-      return null;
-    }
+    $class = Classes::where('id', $this->classid)->first();
+    if ($class != null)
+      return $class->name;
+    return 'Deleted Class';
+  }
 
-    public function getDueDateAttribute($value){
-      return Carbon::parse($this->due)->format('M j');
-    }
+  public function getDueDateAttribute($value) {
+    return Carbon::parse($this->due)->format('M j');
+  }
 
-    public function getDueDateWAttribute($value){
-      return Carbon::parse($this->due)->format('D');
-    }
+  public function getDueDateWAttribute($value) {
+    return Carbon::parse($this->due)->format('D');
+  }
 
-    public function getDueTimeAttribute($value){
-      return Carbon::parse($this->due)->format('g:i A');
-    }
+  public function getDueTimeAttribute($value) {
+    return Carbon::parse($this->due)->format('g:i A');
+  }
 
-    public function getIsDueTodayAttribute($value){
-      if(Carbon::now()->toDateString() == Carbon::parse($this->due)->toDateString()){
-        return true;
-      }
-      return false;
-    }
+  public function getCreatedDateAttribute($value) {
+    return Carbon::parse($this->created_at)->format('M j');
+  }
 
-    public function getIsLateAttribute($value){
-      if(Carbon::now()->toDateString() > Carbon::parse($this->due)->toDateString()){
-        return true;
-      }
-      return false;
-    }
+  public function getEditedDateAttribute($value) {
+    if ($this->created_at != $this->updated_at)
+      return Carbon::parse($this->updated_at)->format('M j');
+    return null;
+  }
 
-    public function getCreatedDateAttribute($value){
-      return Carbon::parse($this->created_at)->format('M j');
-    }
+  public function reminders() {
+    return $this->hasMany(AssignmentReminder::class);
+  }
 
-    public function getEditedDateAttribute($value){
-      if ($this->created_at != $this->updated_at)
-        return Carbon::parse($this->updated_at)->format('M j');
-      return null;
-    }
+  public function user() {
+    return $this->belongsTo(User::class, 'userid');
+  }
 }
