d433854f6b81fc3eb4ddfd048fc1b1c69b135415
Alex Batts <alex.batts05@gmail.com>
2021-10-01 20:15:56 -0400

    Link to set class schedule on dashboard

--- resources/views/livewire/dashboard/dashboard-header.blade.php
+++ resources/views/livewire/dashboard/dashboard-header.blade.php
@@ -1,15 +1,15 @@
 <div class="" wire:poll.300000ms="refresh">
-  <div class="mdc-card mdc-card--outlined py-8 px-6 md:px-10 text-white" style="background-color: #242323">
+  <div class="px-6 py-8 text-white mdc-card mdc-card--outlined md:px-10" style="background-color: #242323">
     <div>
-      <p class="text-4xl md:text-6xl font-semibold float-left">{{$greeting}} {{Auth::User()->firstname}}!</p>
+      <p class="float-left text-4xl font-semibold md:text-6xl">{{$greeting}} {{Auth::User()->firstname}}!</p>
       <div class="float-right">
-        <button class="mdc-icon-button material-icons -mt-3" aria-describedby="update-label">
+        <a class="-mt-3 mdc-icon-button material-icons" aria-describedby="update-label" href="{{route('schedule-settings')}}">
           <div class="mdc-icon-button__ripple"></div>
           settings_suggest
-        </button>
+        </a>
       </div>
     </div>
-    <p class="mt-3 text-xl md:text-2xl font-medium">Today is {{Carbon::now()->format('l, F jS')}}</p>
+    <p class="mt-3 text-xl font-medium md:text-2xl">Today is {{Carbon::now()->format('l, F jS')}}</p>
     <p class="mt-4">{{$dayOfWeekString}}</p>
   </div>
   <x-ui.tooltip tooltip-id="update-label" text="Change the schedule block for today"/>
