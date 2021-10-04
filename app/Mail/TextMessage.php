69c275640923c4e9e5ed77e7841807b4dca8bc42
Alex Batts <alex.batts05@gmail.com>
2021-10-04 18:23:53 -0400

    Bug and routing fixes

--- app/Mail/TextMessage.php
+++ app/Mail/TextMessage.php
@@ -8,47 +8,48 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Auth;
 
-class TextMessage extends Mailable
-{
+class TextMessage extends Mailable {
 
-    use Queueable, SerializesModels;
+  use Queueable, SerializesModels;
 
-    public $subject = '';
+  public $subject = '';
 
-    public $messageText;
+  public $messageText;
 
-    protected $carriers = [
-      'Verizon Wireless' => '@vtext.com',
-      'T-Mobile' => '@tmomail.net',
-      'AT+T Mobility' => '@txt.att.net',
-      'Sprint' => '@messaging.sprintpcs.com'
-    ];
+  protected $carriers = [
+    'Verizon Wireless' => '@vtext.com',
+    'T-Mobile' => '@tmomail.net',
+    'T-Mobile USA, Inc.' => '@tmomail.net',
+    'AT&T Wireless' => '@txt.att.net',
+    'Sprint' => '@messaging.sprintpcs.com',
+    'Google (Grand Central) BWI - Bandwidth.com - SVR' => null,
 
-    protected function buildSubject($message)
-    {
-        $message->subject($this->subject);
+  ];
 
-        return $this;
-    }
+  protected function buildSubject($message) {
+    $message->subject($this->subject);
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct($message) {
-        $this->messageText = $message;
-    }
+    return $this;
+  }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build() {
-      return $this->from('reminders@schedulist.xyz', '')
-                  ->subject(' ')
-                  ->replyTo('reminders@schedulist.xyz', 'reminders@schedulist.xyz')
-                  ->view('emails.text_message');
-    }
+  /**
+   * Create a new message instance.
+   *
+   * @return void
+   */
+  public function __construct($message) {
+    $this->messageText = $message;
+  }
+
+  /**
+   * Build the message.
+   *
+   * @return $this
+   */
+  public function build() {
+    return $this->from('reminders@schedulist.xyz', '')
+      ->subject(' ')
+      ->replyTo('reminders@schedulist.xyz', 'reminders@schedulist.xyz')
+      ->view('emails.text_message');
+  }
 }
