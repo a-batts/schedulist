592c37e4088810ef5d61eb5d32190ac39d86751a
Alex Batts <alex.batts05@gmail.com>
2021-10-10 00:13:39 -0400

    Social Logins component redesign

--- app/Http/Livewire/Profile/SocialLogins.php
+++ app/Http/Livewire/Profile/SocialLogins.php
@@ -6,21 +6,24 @@ use Livewire\Component;
 use Auth;
 use App\Models\User;
 
-class SocialLogins extends Component
-{
-    /**
-     * Remove Google account from user
-     * @return [type]
-     */
-    public function disconnectGoogle(){
-      $user = Auth::user();
-      $user->google_email = null;
-      $user->google_id = "0";
-      $user->save();
-      $this->emit('toastMessage', 'Sucessfully disconnected Google Account');
-    }
+class SocialLogins extends Component {
+  /**
+   * Remove Google account from user
+   * @return void
+   */
+  public function disconnectGoogle() {
+    $user = Auth::user();
+    $user->google_email = null;
+    $user->google_id = "0";
+    $user->save();
+    $this->emit('toastMessage', 'Sucessfully disconnected Google Account');
+  }
 
-    public function render(){
-        return view('livewire.profile.social-logins');
-    }
+  public function getHasGoogleAccountProperty() {
+    return !(Auth::user()->google_id == "0" || Auth::user()->google_id == null);
+  }
+
+  public function render() {
+    return view('livewire.profile.social-logins');
+  }
 }
