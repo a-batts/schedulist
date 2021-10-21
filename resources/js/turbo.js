3aad75d9215ccc660b54f5574903dab4510a8a46
Alex Batts <alex.batts05@gmail.com>
2021-10-21 16:56:57 -0400

    Updated resources with new MDC components version

--- resources/js/turbo.js
+++ resources/js/turbo.js
@@ -7,18 +7,21 @@ window.Turbo = Turbolinks;
 Turbo.setProgressBarDelay(700);
 
 document.addEventListener("turbolinks:before-cache", function () {
-  var hide = [].map.call(document.querySelectorAll('.mdc-button__ripple'), function(el) {
+  var hide = [].map.call(document.querySelectorAll('.mdc-button__ripple'), function (el) {
     el.style.display = 'none';
   });
-  var hide = [].map.call(document.querySelectorAll('.modalCreate'), function(el) {
+  var hide = [].map.call(document.querySelectorAll('.modalCreate'), function (el) {
     el.style.display = 'none';
   });
-  var hide = [].map.call(document.querySelectorAll('.add_class_div'), function(el) {
+  var hide = [].map.call(document.querySelectorAll('.add_class_div'), function (el) {
     el.style.display = 'none';
   });
-  var hide = [].map.call(document.querySelectorAll('.profile_menu'), function(el) {
+  var hide = [].map.call(document.querySelectorAll('.profile_menu'), function (el) {
     el.style.display = 'none';
   });
+  var hide = [].map.call(document.querySelectorAll('.mdc-deprecated-list-item__ripple'), function (el) {
+    el.style.display = 'none';
+  })
   enableScroll();
 });
 
