c65a14b1c4fd6eac9960037ef42c0ceef6474a0b
Alex Batts <alex.batts05@gmail.com>
2022-05-24 16:14:01 -0400

    New themes page design, updated resources

--- resources/views/livewire/themes.blade.php
+++ resources/views/livewire/themes.blade.php
@@ -1,5 +1,5 @@
-<x-app-layout title="Theme Settings">
-  <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8" id="app">
+<x-app-layout title="Theme">
+  <div class="py-10 mx-auto max-w-7xl sm:px-6 lg:px-8" id="app">
     <div class="mt-10"></div>
     @livewire('profile.themes.change-themes')
   </div>
