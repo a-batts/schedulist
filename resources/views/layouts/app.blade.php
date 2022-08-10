cffa16b015653caee481a3e8dce659e200dd293f
Alex Batts <=>
2022-08-09 23:27:34 -0400

    Temporairly disabled turbolinks until working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
     @livewireStyles
 
     <!-- Scripts -->
-    <script src="{{ mix('js/turbo.js') }}"></script>
+    <!-- <script src="{{ mix('js/turbo.js') }}"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.js"></script>
   </head>
