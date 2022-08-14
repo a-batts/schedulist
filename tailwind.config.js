5c44de5373ee29d097cc223233fe347950151c4f
Shift <shift@laravelshift.com>
2022-08-14 18:45:51 +0000

    Streamline Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 module.exports = {
-  purge: [
-    './vendor/laravel/jetstream/**/*.blade.php',
-    './storage/framework/views/*.php',
-    './resources/views/**/*.blade.php',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    opacity: ['responsive', 'hover', 'focus', 'disabled'],
-    extend: {},
-  },
-  plugins: [],
+    purge: [
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
 }
