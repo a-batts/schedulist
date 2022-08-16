4a03674dcfef25597756148c05bf37a38f06ff4e
Alex Batts <alex.batts05@gmail.com>
2022-08-16 15:25:06 -0400

    Select label no longer glitches with presel val

--- app/View/Components/UI/Select.php
+++ app/View/Components/UI/Select.php
@@ -5,11 +5,41 @@ namespace App\View\Components\Ui;
 use Illuminate\View\Component;
 
 class Select extends Component {
-  public $data;
-  public $title;
-  public $style;
-  public $bind;
-  public $value;
+
+  /**
+   * The select title
+   *
+   * @var string
+   */
+  public string $title;
+
+  /**
+   * The data to populate the select with
+   *
+   * @var string
+   */
+  public string $data;
+
+  /**
+   * The style to apply for the select component (accepts either outlined or filled)
+   *
+   * @var string
+   */
+  public string $style;
+
+  /**
+   * The Alpine variable to bind to
+   *
+   * @var string
+   */
+  public string $bind;
+
+  /**
+   * The initial value for the select
+   *
+   * @var string|null
+   */
+  public ?string $value;
 
   /**
    * Create a new component instance.
