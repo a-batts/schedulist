1f82926149d0503ec05c718ea74b40a5b1b71743
Alex Batts <alex.batts05@gmail.com>
2022-08-16 16:53:16 -0400

    Moved color picker, color changing now uses ajax

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class EventController extends Controller {
+
+    public function setColor(Request $request) {
+        $validColors = Event::VALID_COLORS;
+        $color = $request->color;
+
+        $event = Event::find($request->id);
+        if ($request->user() && $event->owner == $request->user()->id) {
+            if (in_array($color, $validColors)) {
+                $event->color = $color;
+                $event->save();
+                return response()->json(['success' => 'Color updated'], 200);
+            }
+            return response()->json(['error' => 'An invalid color option was provided'], 400);
+        } else {
+            return response()->json(['error' => 'This request can only be made by the owner of the event'], 401);
+        }
+    }
+}
--- resources/views/livewire/schedule/agenda-color-picker.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-  <!-- This is just an empty component to handle the color swapping method -->
-</div>
