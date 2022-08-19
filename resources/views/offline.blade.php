5ed212302ad22c57eaa4518e5b255d36a7479de5
Alex Batts <alex.batts05@gmail.com>
2022-08-18 21:10:19 -0400

    Offline view bug fixed

--- resources/views/offline.blade.php
+++ resources/views/offline.blade.php
@@ -6,7 +6,6 @@
     <title>No Connection</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet" type="text/css" media="print" onload="this.media='all'">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@700&display=swap" rel="stylesheet" type="text/css" media="print" onload="this.media='all'">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css" media="print" onload="this.media='all'">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/logo/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/logo/favicon-32x32.png') }}">
@@ -15,7 +14,6 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/bundle.css') }}">
-    <link rel="stylesheet" href="{{ mix('css/styles.css') }}">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
     <!-- Scripts -->
@@ -46,8 +44,8 @@
       <main>
         <div class="center">
           <span class="material-icons wifi_icon mdc-typography">signal_wifi_off</span>
-          <p class="mdc-typography mdc-typography--headline2 center_text mt-5 text-center font-bold">You're offline</p>
-          <p class="mdc-typography mt-5 text-center text-lg font-medium text-gray-600">We'll try to reconnect you when you get back online</p>
+          <p class="mt-5 font-bold text-center mdc-typography mdc-typography--headline2 center_text">You're offline</p>
+          <p class="mt-5 text-lg font-medium text-center text-gray-600 mdc-typography">We'll try to reconnect you when you get back online</p>
         </div>
       </main>
     </div>
