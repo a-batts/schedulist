c75fa360c9bfae10af88c9df29fbc13913d077eb
Alex Batts <alex.batts05@gmail.com>
2022-08-21 14:56:46 -0400

    Updated assignment list components

--- app/Http/Livewire/Assignments/AssignmentList.php
+++ app/Http/Livewire/Assignments/AssignmentList.php
@@ -15,37 +15,75 @@ use Illuminate\Support\Facades\Crypt;
 use Livewire\Component;
 
 class AssignmentList extends Component {
-  public $assignments;
 
+  /**
+   * Array of user's assignments
+   *
+   * @var array
+   */
+  public array $assignments;
+
+  /**
+   * Array of user's created classes
+   *
+   * @var array
+   */
+  public array $classes = [];
+
+  /**
+   * The selected class provided by URL paramater
+   *
+   * @var Classes|int|null
+   */
   public $class;
 
-  public $classes = [];
+  /**
+   * The selected completion filter provided by URL paramater
+   *
+   * @var string
+   */
+  public string $due = 'Incomplete';
 
-  public $due = 'Incomplete';
-
-  public $filters = 'Incomplete,Completed';
+  /**
+   * Valid completion filter options
+   *
+   * @var array
+   */
+  public array $filters = ['Incomplete', 'Completed'];
 
   protected $listeners = ['refreshAssignments'];
 
-  public function mount() {
-    $classes = Classes::where('userid', Auth::User()->id)->get();
-    foreach ($classes as $class) {
-      array_push($this->classes, ['id' => $class->id, 'name' => $class->name]);
-    }
+  /**
+   * Mount the component
+   *
+   * @return void
+   */
+  public function mount(): void {
+    $usersClasses = Classes::where('userid', Auth::User()->id)->get();
+    foreach ($usersClasses as $class)
+      $this->classes[] = ['id' => $class->id, 'name' => $class->name];
+
     if ($this->class != -1)
       $this->class = Classes::where('id', (int) $this->class)->where('userid', Auth::User()->id)->first()->id;
 
     $this->assignments = $this->getAssignments();
   }
 
-  public function getAssignments() {
+  /**
+   * Return array of the user's assignmetns
+   *
+   * @return array
+   */
+  public function getAssignments(): array {
     $assignments = Assignment::where('userid', Auth::User()->id)->orderBy('due', 'asc')->get()->toArray();
     foreach ($assignments as $key => $value) {
-      $assignments[$key]['assignment_name'] = Crypt::decryptString($value['assignment_name']);
       $due = Carbon::parse($value['due']);
+
+      $assignments[$key]['assignment_name'] = Crypt::decryptString($value['assignment_name']);
       $assignments[$key]['due_time'] = $due->format('g:i A');
-      $assignments[$key]['due_date'] = $due->format('M j');
+      $assignments[$key]['due_date'] = $due->format('M j, Y');
       $assignments[$key]['description'] = Crypt::decryptString($value['description']);
+
       if ($value['classid'] != null) {
         $k = array_search($value['classid'], array_column($this->classes, 'id'));
         if ($k === false)
@@ -63,24 +101,57 @@ class AssignmentList extends Component {
     return $assignments;
   }
 
-  public function refreshAssignments() {
+  /**
+   * Return the name of a specific class
+   *
+   * @param int $id
+   * @return string
+   */
+  public function getClassName($id): string {
+    if ($id >= 0) {
+      foreach ($this->classes as $class) {
+        if ($class['id'] == $id)
+          return $class['name'];
+      }
+    }
+
+    return '';
+  }
+
+  /**
+   * Update the user's assignments
+   *
+   * @return void
+   */
+  public function refreshAssignments(): void {
     $this->assignments = $this->getAssignments();
     $this->dispatchBrowserEvent('update-assignments');
   }
 
-  public function updateStatus($id) {
+  /**
+   * Mark an assignment as complete/incomplete
+   *
+   * @param int $id
+   * @return void
+   */
+  public function updateStatus($id): void {
     $assignment = Assignment::findOrFail($id);
+
+    //Only allow user to modify an assignment they own
     if ($assignment->userid == Auth::User()->id) {
-      if ($assignment->status == 'inc')
-        $assignment->status = 'done';
-      else
-        $assignment->status = 'inc';
+      $assignment->status = $assignment->status == 'inc' ? 'done' : 'inc';
+
       $assignment->save();
       $this->refreshAssignments();
       $this->emit('toastMessage', 'Marked assignment as ' . ($assignment->status == 'done' ? 'done' : 'incomplete'));
     }
   }
 
+  /**
+   * Mount the component
+   *
+   * @return Illuminate\Contracts\View\View|Illuminate\Contracts\View\Factory
+   */
   public function render() {
     return view('livewire.assignments.assignment-list');
   }
