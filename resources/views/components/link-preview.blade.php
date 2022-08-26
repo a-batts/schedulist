0772b71ac045ad28bc1266319f49c476a0636127
Alex Batts <alex.batts05@gmail.com>
2022-08-26 17:33:24 -0400

    Link preview ripple restored

--- resources/views/components/link-preview.blade.php
+++ resources/views/components/link-preview.blade.php
@@ -1,11 +1,12 @@
-<div class="mdc-card mdc-card--outlined assignment-link">
+<div class="mdc-card mdc-card--outlined">
   <a href="{{$preview->getLink()}}" target="_blank">
     <div class="mdc-card__primary-action link__p-a" tabindex="0">
       <div class="link-primary-text left-28 mt-2.5">
-        <h2 class="mdc-typography--headline6 assignment-link-title ml-1 truncate text-xl font-medium">{{$preview->getText()}}</h2>
-        <h3 class="mdc-typography--subtitle1 assignment-link-url ml-1 truncate text-xs text-gray-600">{{$preview->getLink()}}</h3>
+        <h2 class="ml-1 text-xl font-medium truncate mdc-typography--headline6 assignment-link-title">{{$preview->getText()}}</h2>
+        <h3 class="ml-1 text-xs text-gray-600 truncate mdc-typography--subtitle1 assignment-link-url">{{$preview->getLink()}}</h3>
       </div>
       <div class="mdc-card__media mdc-card__media--square assignment-link-image" {{$preview->getPreview()}}></div>
+      <div class="mdc-card__ripple"></div>
     </div>
   </a>
 </div>
