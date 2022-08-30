d797bd502b6b212aad0c34edd652626683660489
Alex Batts <alex.batts05@gmail.com>
2022-08-30 17:22:24 -0400

    Clean up old class schedule tables

---
+++ database/migrations/2022_08_30_171713_delete_class_schedule_user_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DeleteClassScheduleUserTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::drop('class_schedule_user');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        //
+    }
+}
