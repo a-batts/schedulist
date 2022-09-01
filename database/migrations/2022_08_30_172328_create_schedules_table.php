5faa0d2804c2f27cd63160cffd447300f4ff54d1
Alex Batts <alex.batts05@gmail.com>
2022-09-01 15:33:17 -0400

    Migrations to create the new tables for schedules

---
+++ database/migrations/2022_08_30_172328_create_schedules_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('schedules');
+    }
+}
---
+++ database/migrations/2022_08_30_172507_create_new_class_times_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewClassTimesTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('class_times', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('schedule_id')->constrained();
+            $table->foreignId('class_id')->constrained();
+            $table->integer('day_of_week');
+            $table->string('start_time', 5);
+            $table->string('end_time', 5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('new_class_times');
+    }
+}
