554c03ac0101b2a170d1befc3d476c2cb4783f8c
Alex Batts <alex.batts05@gmail.com>
2022-09-01 15:36:29 -0400

    New models & class schedule helper for new tables

--- app/Models/Classes.php
+++ app/Models/Classes.php
@@ -4,16 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
-class Classes extends Model
-{
-    use HasFactory;
+class Classes extends Model {
+  use HasFactory;
 
-    protected $table = 'classes';
+  protected $table = 'classes';
 
-    protected $guarded = ['id', 'userid'];
+  protected $guarded = ['id', 'userid'];
 
-    public function links(){
-      return $this->hasMany(ClassLink::class, 'class_id');
-    }
+  public function getTeacherNameAttribute() {
+    return isset($this->teacher) ? Crypt::decryptString($this->teacher) : '';
+  }
+
+  public function getLocationAttribute() {
+    return isset($this->class_location) ? Crypt::decryptString($this->class_location) : '';
+  }
+
+  public function links() {
+    return $this->hasMany(ClassLink::class, 'class_id');
+  }
 }
