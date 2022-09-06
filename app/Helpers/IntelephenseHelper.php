bcb8ea4c6471f970c34c836859c05c37057392db
Alex Batts <alex.batts05@gmail.com>
2022-09-06 12:16:10 -0400

    Fix for errors resulting from livewire layout()

---
+++ app/Helpers/IntelephenseHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Illuminate\Contracts\View;
+
+use Illuminate\Contracts\Support\Renderable;
+
+interface View extends Renderable {
+    /** @return static */
+    public function layout();
+
+    /** 
+     * @param array $data
+     * @return static
+     * 
+     */
+    public function layoutData();
+}
