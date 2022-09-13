2382ed49ef4c0f17037a33b10e532554e3c16bd4
Alex Batts <alex.batts05@gmail.com>
2022-09-13 11:29:54 -0400

    Fixed view settings button

--- resources/views/livewire/profile/manage-data.blade.php
+++ resources/views/livewire/profile/manage-data.blade.php
@@ -1,29 +1,33 @@
 <div>
     <x-ui.settings-card title="Your data"
     description="Download or delete parts of your data, or delete your entire account.">
-        <div class="-mx-4 pb-2">
-            <ul class="mdc-deprecated-list mdc-deprecated-list--two-line mdc-deprecated-list--icon-list security-list w-full">
-                <a class="security-list mdc-deprecated-list-item border-b border-gray-200" tabindex="0" href="{{route('account.manage-data')}}">
+        <div class="pb-2 -mx-4">
+            <ul class="w-full mdc-deprecated-list mdc-deprecated-list--two-line mdc-deprecated-list--icon-list security-list">
+                <a class="border-b border-gray-200 security-list mdc-deprecated-list-item" tabindex="0" href="{{route('account.manage-data')}}">
                     <span class="mdc-deprecated-list-item__ripple"></span>
-                    <span class="mdc-deprecated-list-item__graphic material-icons text-gray-600 text-inherit" aria-hidden="true">
+                    <span class="text-gray-600 mdc-deprecated-list-item__graphic material-icons text-inherit" aria-hidden="true">
                         source
                     </span>
                     <span class="security-list-text mdc-deprecated-list-item__text">
                         <span class="text-lg">Manage data</span>
-                        <span class="mdc-deprecated-list-item__secondary-text -mt-2 text-sm text-gray-500">Download archives of your Schedulist data or delete specific parts of it</span>
+                        <span class="-mt-2 text-sm text-gray-500 mdc-deprecated-list-item__secondary-text">Download archives of your Schedulist data or delete specific parts of it</span>
                     </span>
-                    <button class="mdc-icon-button material-icons absolute right-0 sm:right-5" type="button" aria-describedby="" aria-label="">navigate_next</button>
+                    <button class="absolute right-0 mdc-icon-button material-icons text-primary sm:right-5" type="button" aria-describedby="" aria-label="">
+                        <div class="mdc-icon-button__ripple"></div>
+                        navigate_next</button>
                 </a>
-                <a class="security-list mdc-deprecated-list-item border-b border-gray-200" tabindex="0" href="{{route('account.delete-account')}}">
+                <a class="border-b border-gray-200 security-list mdc-deprecated-list-item" tabindex="0" href="{{route('account.delete-account')}}">
                     <span class="mdc-deprecated-list-item__ripple"></span>
-                    <span class="mdc-deprecated-list-item__graphic material-icons text-gray-600 text-inherit" aria-hidden="true">
+                    <span class="text-gray-600 mdc-deprecated-list-item__graphic material-icons text-inherit" aria-hidden="true">
                         delete_forever
                     </span>
                     <span class="security-list-text mdc-deprecated-list-item__text">
                         <span class="text-lg">Delete account</span>
-                        <span class="mdc-deprecated-list-item__secondary-text -mt-2 text-sm text-gray-500">Permanently delete your account and all of its data</span>
+                        <span class="-mt-2 text-sm text-gray-500 mdc-deprecated-list-item__secondary-text">Permanently delete your account and all of its data</span>
                     </span>
-                    <button class="mdc-icon-button material-icons absolute right-0 sm:right-5" type="button" aria-describedby="" aria-label="">navigate_next</button>
+                    <button class="absolute right-0 mdc-icon-button material-icons text-primary sm:right-5" type="button" aria-describedby="" aria-label="">
+                        <div class="mdc-icon-button__ripple"></div>
+                        navigate_next</button>
                 </a>
               </ul>
         </div>
--- resources/views/livewire/profile/security-settings.blade.php
+++ resources/views/livewire/profile/security-settings.blade.php
@@ -1,53 +1,61 @@
 <div>
     <x-ui.settings-card title="Account security"
     description="{{Auth::user()->hasPassword ? 'Change your password, or enable other settings to help keep your account secure.' : 'Set a password first before you can access other security settings.'}}">
-        <div class="-mx-4 pb-2">
-            <ul class="mdc-deprecated-list mdc-deprecated-list--two-line mdc-deprecated-list--icon-list security-list w-full">
+        <div class="pb-2 -mx-4">
+            <ul class="w-full mdc-deprecated-list mdc-deprecated-list--two-line mdc-deprecated-list--icon-list security-list">
                 @if(Auth::user()->hasPassword)
-                    <a class="security-list mdc-deprecated-list-item border-b border-gray-200" tabindex="0" href="{{route('account.update-password')}}">
+                    <a class="border-b border-gray-200 security-list mdc-deprecated-list-item" tabindex="0" href="{{route('account.update-password')}}">
                         <span class="mdc-deprecated-list-item__ripple"></span>
-                        <span class="mdc-deprecated-list-item__graphic material-icons text-gray-600 text-inherit" aria-hidden="true">
+                        <span class="text-gray-600 mdc-deprecated-list-item__graphic material-icons text-inherit" aria-hidden="true">
                             vpn_key
                         </span>
                         <span class="security-list-text mdc-deprecated-list-item__text">
                             <span class="text-lg">Change your password</span>
-                            <span class="mdc-deprecated-list-item__secondary-text -mt-2 text-sm text-gray-500">Choose a new strong password</span>
+                            <span class="-mt-2 text-sm text-gray-500 mdc-deprecated-list-item__secondary-text">Choose a new strong password</span>
                         </span>
-                        <button class="mdc-icon-button material-icons absolute right-0 sm:right-5" type="button" aria-describedby="" aria-label="">navigate_next</button>
+                        <button class="absolute right-0 mdc-icon-button material-icons text-primary sm:right-5" type="button" aria-describedby="" aria-label="">
+                            <div class="mdc-icon-button__ripple"></div>
+                            navigate_next</button>
                     </a>
-                    <a class="security-list mdc-deprecated-list-item border-b border-gray-200" href="{{route('account.manage-devices')}}">
+                    <a class="border-b border-gray-200 security-list mdc-deprecated-list-item" href="{{route('account.manage-devices')}}">
                         <span class="mdc-deprecated-list-item__ripple"></span>
-                        <span class="mdc-deprecated-list-item__graphic material-icons text-gray-600 text-inherit" aria-hidden="true">
+                        <span class="text-gray-600 mdc-deprecated-list-item__graphic material-icons text-inherit" aria-hidden="true">
                             devices
                         </span>
                         <span class="security-list-text mdc-deprecated-list-item__text">
                             <span class="text-lg">Managed signed in devices</span>
-                            <span class="mdc-deprecated-list-item__secondary-text -mt-2 text-sm text-gray-500">View and remove other devices</span>
+                            <span class="-mt-2 text-sm text-gray-500 mdc-deprecated-list-item__secondary-text">View and remove other devices</span>
                         </span>
-                        <button class="mdc-icon-button material-icons absolute right-0 sm:right-5" type="button" aria-describedby="" aria-label="">navigate_next</button>
+                        <button class="absolute right-0 mdc-icon-button material-icons text-primary sm:right-5" type="button" aria-describedby="" aria-label="">
+                            <div class="mdc-icon-button__ripple"></div>
+                            navigate_next</button>
                     </a>
-                    <a class="security-list mdc-deprecated-list-item border-b border-gray-200" href="{{route('account.two-factor')}}">
+                    <a class="border-b border-gray-200 security-list mdc-deprecated-list-item" href="{{route('account.two-factor')}}">
                         <span class="mdc-deprecated-list-item__ripple"></span>
-                        <span class="mdc-deprecated-list-item__graphic material-icons text-gray-600 text-inherit" aria-hidden="true">
+                        <span class="text-gray-600 mdc-deprecated-list-item__graphic material-icons text-inherit" aria-hidden="true">
                             verified_user
                         </span>
                         <span class="security-list-text mdc-deprecated-list-item__text">
                             <span class="text-lg">{{!empty(Auth::user()->two_factor_secret) ? 'Manage 2-Factor settings' : 'Enable 2-Factor verification'}}</span>
-                            <span class="mdc-deprecated-list-item__secondary-text -mt-2 text-sm text-gray-500">{{!empty(Auth::user()->two_factor_secret) ? 'Enabled: Manage 2FA settings' : 'Disabled: Setup Schedulist with your 2FA app'}}</span>
+                            <span class="-mt-2 text-sm text-gray-500 mdc-deprecated-list-item__secondary-text">{{!empty(Auth::user()->two_factor_secret) ? 'Enabled: Manage 2FA settings' : 'Disabled: Setup Schedulist with your 2FA app'}}</span>
                         </span>
-                        <button class="mdc-icon-button material-icons absolute right-0 sm:right-5" type="button" aria-describedby="" aria-label="">navigate_next</button>
+                        <button class="absolute right-0 mdc-icon-button material-icons text-primary sm:right-5" type="button" aria-describedby="" aria-label="">
+                            <div class="mdc-icon-button__ripple"></div>
+                            navigate_next</button>
                     </a>
                 @else
-                <a class="security-list mdc-deprecated-list-item border-b border-gray-200" tabindex="0" href="{{route('account.set-password')}}">
+                <a class="border-b border-gray-200 security-list mdc-deprecated-list-item" tabindex="0" href="{{route('account.set-password')}}">
                     <span class="mdc-deprecated-list-item__ripple"></span>
-                    <span class="mdc-deprecated-list-item__graphic material-icons text-gray-600 text-inherit" aria-hidden="true">
+                    <span class="text-gray-600 mdc-deprecated-list-item__graphic material-icons text-inherit" aria-hidden="true">
                         vpn_key
                     </span>
                     <span class="security-list-text mdc-deprecated-list-item__text">
                         <span class="text-lg">Set a password</span>
-                        <span class="mdc-deprecated-list-item__secondary-text -mt-2 text-sm text-gray-500">Use it as a backup to your Google login</span>
+                        <span class="-mt-2 text-sm text-gray-500 mdc-deprecated-list-item__secondary-text">Use it as a backup to your Google login</span>
                     </span>
-                    <button class="mdc-icon-button material-icons absolute right-0 sm:right-5" type="button" aria-describedby="" aria-label="">navigate_next</button>
+                    <button class="absolute right-0 mdc-icon-button material-icons text-primary sm:right-5" type="button" aria-describedby="" aria-label="">
+<div class="mdc-icon-button__ripple"></div>
+navigate_next</button>
                 </a>
                 @endif
               </ul>
