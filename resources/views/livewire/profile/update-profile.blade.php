82ce06cb7d73231d5116468103ca0740a490b508
Alex Batts <alex.batts05@gmail.com>
2022-09-15 21:11:26 -0400

    Updated input titles

--- resources/views/livewire/profile/update-profile.blade.php
+++ resources/views/livewire/profile/update-profile.blade.php
@@ -15,14 +15,14 @@ class="" id="profile_settings">
     description="Update your basic information, such as your name and email address.">
     <div class="pt-4 pb-2">
       <p class="mt-1 text-gray-600">Set or change your profile picture</p>
-      <p class="mt-1 text-sm text-gray-500"><span class="inline-block"><span class="material-icons mt-1 mr-3 inline-block align-text-bottom text-inherit" style="vertical-align: -5px">public</span>Other Schedulist users will be able to view the picture you select.</span></p>
+      <p class="mt-1 text-sm text-gray-500"><span class="inline-block"><span class="inline-block mt-1 mr-3 align-text-bottom material-icons text-inherit" style="vertical-align: -5px">public</span>Other Schedulist users will be able to view the picture you select.</span></p>
       <div>
-        <div class="mt-5 block w-full">
+        <div class="block w-full mt-5">
           <div class="w-full">
-            <img class="mx-auto h-24 w-24 rounded-full" src="{{Auth::User()->profile_photo_url}}"/>
+            <img class="w-24 h-24 mx-auto rounded-full" src="{{Auth::User()->profile_photo_url}}"/>
           </div>
-          <div class="mt-7 block text-center">
-            <button class="mdc-button mdc-button--outlined mdc-button-ripple inline-block" type="button" @click="$dispatch('open-photo-dialog')" wire:ignore>
+          <div class="block text-center mt-7">
+            <button class="inline-block mdc-button mdc-button--outlined mdc-button-ripple" type="button" @click="$dispatch('open-photo-dialog')" wire:ignore>
               <i class="material-icons mdc-button__icon" aria-hidden="true">edit</i>
               <span class="mdc-button__ripple"></span>Change photo
             <button>
@@ -33,11 +33,11 @@ class="" id="profile_settings">
     <x-jet-section-border />
     <div class="mt-2">
       <div class="float-left w-1/2 pr-1.5">
-        <label class="mdc-text-field mdc-text-field--outlined mdc-text-field--label-floating w-full" x-bind:class="{'mdc-text-field--invalid': errorMessages['state.firstname'] != undefined}" wire:ignore>
+        <label class="w-full mdc-text-field mdc-text-field--outlined mdc-text-field--label-floating" x-bind:class="{'mdc-text-field--invalid': errorMessages['state.firstname'] != undefined}" wire:ignore>
           <span class="mdc-notched-outline">
             <span class="mdc-notched-outline__leading"></span>
             <span class="mdc-notched-outline__notch">
-              <span class="mdc-floating-label mdc-floating-label--float-above" id="first-name-label">First Name</span>
+              <span class="mdc-floating-label mdc-floating-label--float-above" id="first-name-label">First name</span>
             </span>
             <span class="mdc-notched-outline__trailing"></span>
           </span>
@@ -46,11 +46,11 @@ class="" id="profile_settings">
         <x-ui.validation-error for="state.firstname"/>
       </div>
       <div class="float-right w-1/2 pl-1.5">
-        <label class="mdc-text-field mdc-text-field--outlined mdc-text-field--label-floating w-full" x-bind:class="{'mdc-text-field--invalid': errorMessages['state.lastname'] != undefined}" wire:ignore>
+        <label class="w-full mdc-text-field mdc-text-field--outlined mdc-text-field--label-floating" x-bind:class="{'mdc-text-field--invalid': errorMessages['state.lastname'] != undefined}" wire:ignore>
           <span class="mdc-notched-outline">
             <span class="mdc-notched-outline__leading"></span>
             <span class="mdc-notched-outline__notch">
-              <span class="mdc-floating-label mdc-floating-label--float-above" id="last-name-label">Last Name</span>
+              <span class="mdc-floating-label mdc-floating-label--float-above" id="last-name-label">Last name</span>
             </span>
             <span class="mdc-notched-outline__trailing"></span>
           </span>
@@ -59,7 +59,7 @@ class="" id="profile_settings">
         <x-ui.validation-error for="state.lastname"/>
       </div>
       </div>
-      <label class="mdc-text-field mdc-text-field--outlined mdc-text-field--label-floating w-full" x-bind:class="{'mdc-text-field--invalid': errorMessages['state.email'] != undefined}" wire:ignore>
+      <label class="w-full mdc-text-field mdc-text-field--outlined mdc-text-field--label-floating" x-bind:class="{'mdc-text-field--invalid': errorMessages['state.email'] != undefined}" wire:ignore>
         <span class="mdc-notched-outline">
           <span class="mdc-notched-outline__leading"></span>
           <span class="mdc-notched-outline__notch">
@@ -72,11 +72,11 @@ class="" id="profile_settings">
       <x-ui.validation-error for="state.email"/>
       <div class="w-full">
         <div class="float-left w-1/2 pr-1.5">
-          <label class="mdc-text-field mdc-text-field--outlined mdc-text-field--label-floating w-full" x-bind:class="{'mdc-text-field--invalid': errorMessages['state.phone'] != undefined}" wire:ignore>
+          <label class="w-full mdc-text-field mdc-text-field--outlined mdc-text-field--label-floating" x-bind:class="{'mdc-text-field--invalid': errorMessages['state.phone'] != undefined}" wire:ignore>
             <span class="mdc-notched-outline">
               <span class="mdc-notched-outline__leading"></span>
               <span class="mdc-notched-outline__notch">
-                <span class="mdc-floating-label mdc-floating-label--float-above" id="phone-label">Phone Number</span>
+                <span class="mdc-floating-label mdc-floating-label--float-above" id="phone-label">Phone number</span>
               </span>
               <span class="mdc-notched-outline__trailing"></span>
             </span>
@@ -87,11 +87,11 @@ class="" id="profile_settings">
       </div>
       <div>
         <div class="float-left clear-left w-1/2 pr-1.5">
-          <label class="mdc-text-field mdc-text-field--outlined mdc-text-field--label-floating w-full" x-bind:class="{'mdc-text-field--invalid': errorMessages['state.school'] != undefined}" wire:ignore>
+          <label class="w-full mdc-text-field mdc-text-field--outlined mdc-text-field--label-floating" x-bind:class="{'mdc-text-field--invalid': errorMessages['state.school'] != undefined}" wire:ignore>
             <span class="mdc-notched-outline">
               <span class="mdc-notched-outline__leading"></span>
               <span class="mdc-notched-outline__notch">
-                <span class="mdc-floating-label mdc-floating-label--float-above" id="school-name-label">School Name</span>
+                <span class="mdc-floating-label mdc-floating-label--float-above" id="school-name-label">School name</span>
               </span>
               <span class="mdc-notched-outline__trailing"></span>
             </span>
@@ -100,12 +100,12 @@ class="" id="profile_settings">
           <x-ui.validation-error for="state.school"/>
         </div>
         <div class="float-right w-1/2 pl-1.5">
-          <div class="mdc-select mdc-select--outlined w-full" wire:ignore>
+          <div class="w-full mdc-select mdc-select--outlined" wire:ignore>
             <div class="mdc-select__anchor">
               <span class="mdc-notched-outline">
                 <span class="mdc-notched-outline__leading"></span>
                 <span class="mdc-notched-outline__notch">
-                  <span id="outlined-select-label" class="mdc-floating-label mdc-floating-label--float-above">Grade Level</span>
+                  <span id="outlined-select-label" class="mdc-floating-label mdc-floating-label--float-above">Grade level</span>
                 </span>
                 <span class="mdc-notched-outline__trailing"></span>
               </span>
@@ -161,7 +161,7 @@ class="" id="profile_settings">
         </div>
       </div>
       <div class="mt-4">
-        <button class="mdc-button mdc-button--raised mdc-button-ripple float-right clear-left" wire:ignore>
+        <button class="float-right clear-left mdc-button mdc-button--raised mdc-button-ripple" wire:ignore>
           <span class="mdc-button__ripple"></span>Save Changes
         <button>
       </div>
@@ -169,15 +169,15 @@ class="" id="profile_settings">
   </form>
   <!-- Phone number confirmation menu -->
   <div>
-    <div class="modal-skim inset-0 hidden bg-gray-500 opacity-75" style="display: none" x-show="numberConfirmationDialog" x-cloak wire:ignore.self></div>
-    <div class="modal-container mdc-typography fixed top-20 h-screen w-screen overflow-y-auto pb-6" x-transition x-show="numberConfirmationDialog" x-cloak wire:ignore.self>
-      <div class="mdc-card mdc-card--outlined modal-card px-6 pb-6">
+    <div class="inset-0 hidden bg-gray-500 opacity-75 modal-skim" style="display: none" x-show="numberConfirmationDialog" x-cloak wire:ignore.self></div>
+    <div class="fixed w-screen h-screen pb-6 overflow-y-auto modal-container mdc-typography top-20" x-transition x-show="numberConfirmationDialog" x-cloak wire:ignore.self>
+      <div class="px-6 pb-6 mdc-card mdc-card--outlined modal-card">
         <h6 class="mt-4">
-          <span class="material-icons ml-1 mr-4 select-none" style="vertical-align: -2px">textsms</span>
+          <span class="ml-1 mr-4 select-none material-icons" style="vertical-align: -2px">textsms</span>
           <span class="mt-4 text-3xl font-medium">Verify your phone number</span>
         </h6>
         <p class="mt-3 text-gray-600">We just sent a text with a confirmation code to {{$formattedPhoneNumber ?? 'null'}}.</p>
-        <p class="text-blue mt-1 text-sm" style="cursor: pointer" @click="numberConfirmationDialog = false" wire:click="cancelValidation()">Wrong phone number?</p>
+        <p class="mt-1 text-sm text-blue" style="cursor: pointer" @click="numberConfirmationDialog = false" wire:click="cancelValidation()">Wrong phone number?</p>
         <label class="mt-7 mdc-text-field mdc-text-field--outlined @error('verificationCodeInput') mdc-text-field--invalid @enderror login-form mdc-text-field--label-floating">
           <input type="number" class="mdc-text-field__input" aria-labelledby="confirmation-code-label" wire:model.debounce.250ms="verificationCodeInput" autocomplete="chrome-off" maxlength="6" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" required>
           <span class="mdc-notched-outline mdc-notched-outline--notched" wire:ignore>
@@ -190,7 +190,7 @@ class="" id="profile_settings">
         </label>
         <x-ui.validation-error for="verificationCodeInput"/>
         <div class="mt-10">
-          <button class="mdc-button mdc-button-ripple tfa-button float-left" wire:ignore wire:click="resendAndCount()" id="countdown-button" x-ref="countdownButton">
+          <button class="float-left mdc-button mdc-button-ripple tfa-button" wire:ignore wire:click="resendAndCount()" id="countdown-button" x-ref="countdownButton">
             <span class="mdc-button__ripple"></span>
               Resend code <span id="countdown-time" x-ref="countdownTime" style="white-space: pre"> (120)</span>
           </button>
@@ -198,7 +198,7 @@ class="" id="profile_settings">
             <span class="mdc-button__ripple"></span>
               Submit
           </button>
-          <button class="mdc-button mdc-button-ripple tfa-button mr-3" @click="numberConfirmationDialog = false" wire:click="cancelValidation()" wire:ignore>
+          <button class="mr-3 mdc-button mdc-button-ripple tfa-button" @click="numberConfirmationDialog = false" wire:click="cancelValidation()" wire:ignore>
             <span class="mdc-button__ripple"></span>
               Cancel
           </button>
