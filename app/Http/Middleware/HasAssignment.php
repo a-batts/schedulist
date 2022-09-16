5201d58ef3fa7c6e06585f149a89d82d20cfdc53
Alex Batts <alex.batts05@gmail.com>
2022-09-15 22:32:27 -0400

    Assignment encrypted cast

--- app/Http/Middleware/HasAssignment.php
+++ app/Http/Middleware/HasAssignment.php
@@ -11,25 +11,23 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
 
-class HasAssignment
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param $assignmentString
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        $assignmentString = $request->route('assignment_string');
-        $assignment = Assignment::where('userid', Auth::user()->id)->where('url_string', $assignmentString)->first();
-        if ($assignment != null){
-          $assignmentTitle = Crypt::decryptString($assignment->assignment_name);
-          view()->share(['assignment' => $assignment->id, 'assignmentTitle' => $assignmentTitle]);
-          return $next($request);
-        }
-        abort(403);
+class HasAssignment {
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @param $assignmentString
+   * @return mixed
+   */
+  public function handle(Request $request, Closure $next) {
+    $assignmentString = $request->route('assignment_string');
+    $assignment = Assignment::where('userid', Auth::user()->id)->where('url_string', $assignmentString)->first();
+    if ($assignment != null) {
+      $assignmentTitle = $assignment->assignment_name;
+      view()->share(['assignment' => $assignment->id, 'assignmentTitle' => $assignmentTitle]);
+      return $next($request);
     }
+    abort(403);
+  }
 }
