fadc9454231335f80761aa255c5a1c85d4f84d1c
Alex <alex.batts05@gmail.com>
2022-12-22 15:33:55 -0500

    Fix event name encryption

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Event;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class EventController extends Controller {
 
