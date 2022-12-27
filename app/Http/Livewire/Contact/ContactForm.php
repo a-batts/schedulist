0c8c3b40bd79e2e91785bb08b410b4c0ba34c050
Alex <alex.batts05@gmail.com>
2022-12-27 13:38:00 -0500

    Fix email password

--- app/Http/Livewire/Contact/ContactForm.php
+++ app/Http/Livewire/Contact/ContactForm.php
@@ -7,112 +7,109 @@ use Auth;
 use Mail;
 use App\Mail\ContactUs;
 
-class ContactForm extends Component
-{
-    public $name;
-    public $email;
-    public $reason;
-    public $message;
+class ContactForm extends Component {
+  public $name;
+  public $email;
+  public $reason;
+  public $message;
 
-    function rules(){
-      return [
-          'name' => 'required',
-          'email' => 'nullable|email',
-          'reason' => 'required',
-          'message' => 'required|max:250'
-      ];
+  function rules() {
+    return [
+      'name' => 'required',
+      'email' => 'nullable|email',
+      'reason' => 'required',
+      'message' => 'required|max:250'
+    ];
+  }
+
+  /**
+   * Mount Component
+   * @return void
+   */
+  public function mount() {
+    if (Auth::check()) {
+      $this->name = Auth::User()->firstname . ' ' . Auth::User()->lastname;
+      $this->email = Auth::User()->email;
+    } else {
+      $this->name = null;
+      $this->email = null;
+    }
+    $this->reason = null;
+    $this->message = null;
+  }
+
+  /**
+   * Validate updated properties
+   * @param  mixed $propertyName
+   * @return void
+   */
+  public function updated($propertyName) {
+    $this->validateOnly($propertyName);
+  }
+
+  /**
+   * Submit contact form
+   * @return void
+   */
+  public function submit() {
+    $this->validate();
+
+    if (!$this->reason) {
+      $this->addError('reason', 'A message reason is required');
+      return;
     }
 
-    /**
-     * Mount Component
-     * @return void
-     */
-    public function mount(){
-      if (Auth::check()){
-        $this->name = Auth::User()->firstname.' '.Auth::User()->lastname;
-        $this->email = Auth::User()->email;
-      }
-      else{
-        $this->name = null;
-        $this->email = null;
-      }
-      $this->reason = null;
-      $this->message = null;
-    }
+    if (!$this->email) {
+      $this->email = 'feedback@schedulist.xyz';
+      $replyName = 'NO REPLY';
+    } else
+      $replyName = $this->name;
 
-    /**
-     * Validate updated properties
-     * @param  mixed $propertyName
-     * @return void
-     */
-    public function updated($propertyName){
-        $this->validateOnly($propertyName);
-    }
+    $mail_data = [
+      'name' => $this->name,
+      'reason' => $this->reason,
+      'message' => $this->message,
+      'sendFrom' => $this->email,
+      'replyName' => $replyName,
+    ];
 
-    /**
-     * Submit contact form
-     * @return void
-     */
-    public function submit(){
-      $this->validate();
+    $this->sendEmail($mail_data);
+  }
 
-      if (! $this->reason){
-        $this->addError('reason', 'A message reason is required');
-        return;
-      }
+  /**
+   * Sends email with form data
+   * @param  array  $data
+   * @return void
+   */
+  public function sendEmail(array $data) {
+    $this->emit('startloading');
+    $transport = (new \Swift_SmtpTransport('smtp.hostinger.com', '587'))
+      ->setEncryption('tls')
+      ->setUsername('noreply@schedulist.xyz')
+      ->setPassword(env('NOREPLY_EMAIL_PASSWORD'));
 
-      if(! $this->email){
-        $this->email = 'feedback@schedulist.xyz';
-        $replyName = 'NO REPLY';
-      }
-      else
-        $replyName = $this->name;
+    $mailer = app(\Illuminate\Mail\Mailer::class);
+    $mailer->setSwiftMailer(new \Swift_Mailer($transport));
 
-      $mail_data = [
-        'name' => $this->name,
-        'reason' => $this->reason,
-        'message' => $this->message,
-        'sendFrom' => $this->email,
-        'replyName' => $replyName,
-      ];
+    $mail = $mailer
+      ->to('mail@schedulist.xyz')
+      ->send(new ContactUs($data));
 
-      $this->sendEmail($mail_data);
-    }
+    $this->message = null;
+    $this->emit('toastMessage', 'Message sent! We\'ll get back to you soon.');
+    $this->dispatchBrowserEvent('disable-send-button');
+    $this->emit('stoploading');
+  }
 
-    /**
-     * Sends email with form data
-     * @param  array  $data
-     * @return void
-     */
-    public function sendEmail(array $data){
-      $this->emit('startloading');
-      $transport = (new \Swift_SmtpTransport('smtp.hostinger.com', '587'))
-        ->setEncryption('tls')
-        ->setUsername('feedback@schedulist.xyz')
-        ->setPassword('***REMOVED***');
+  /**
+   * Sets value of reason property from select component
+   * @param string $value
+   */
+  public function setReason(string $value) {
+    $this->reason = $value;
+  }
 
-        $mailer = app(\Illuminate\Mail\Mailer::class);
-        $mailer->setSwiftMailer(new \Swift_Mailer($transport));
-
-        $mail = $mailer
-            ->to('mail@schedulist.xyz')
-            ->send(new ContactUs($data));
-
-        $this->message = null;
-        $this->emit('toastMessage', 'Message sent! We\'ll get back to you soon.');
-        $this->dispatchBrowserEvent('disable-send-button');
-        $this->emit('stoploading');
-    }
-
-    /**
-     * Sets value of reason property from select component
-     * @param string $value
-     */
-    public function setReason(string $value){
-      $this->reason = $value;
-    }
-
-    public function render(){
-        return view('livewire.contact.contact-form')->layout('layouts.guest', ['title' => 'Contact Us']);
-    }
+  public function render() {
+    return view('livewire.contact.contact-form')->layout('layouts.guest', ['title' => 'Contact Us']);
+  }
 }
