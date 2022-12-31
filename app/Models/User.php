11dcf8e4822c4abb49b28792240cca9541dfa8db
Alex <alex.batts05@gmail.com>
2022-12-31 14:39:34 -0500

    Remove broken filament user trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,23 +4,19 @@ namespace App\Models;
 
 use App\Helpers;
 use App\Helpers\HasProfilePhoto;
-use Filament\Models\Concerns\IsFilamentUser;
-use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Auth;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements FilamentUser {
+class User extends Authenticatable {
   use HasApiTokens;
   use HasFactory;
   use HasProfilePhoto;
   use Notifiable;
   use TwoFactorAuthenticatable;
-  use IsFilamentUser;
 
   /**
    * The attributes that are mass assignable.
