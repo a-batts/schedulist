db181243ec8c01207b1171425fb8749f5f13f4ec
Alex <alex.batts05@gmail.com>
2023-01-06 01:28:03 -0500

    New Grade Level enum

---
+++ app/Enums/User/GradeLevel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Enums\User;
+
+enum GradeLevel: int {
+    case ElementarySchool = 1;
+    case MiddleSchool = 2;
+    case HighSchool = 3;
+    case College = 4;
+    case Other = 0;
+
+    /**
+     * Return the formatted name (converts PascalCase to Sentence Case)
+     *
+     * @return string
+     */
+    public function formattedName(): string {
+        return implode(' ', preg_split('/(?=[A-Z])/', $this->name));
+    }
+}
