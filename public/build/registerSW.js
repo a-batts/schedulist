e80341c4284c814bdbb3b0ec5bb300498f4a8a7f
Alex <alex.batts05@gmail.com>
2023-01-08 02:05:11 -0500

    Update vite config

--- public/build/registerSW.js
+++ public/build/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/build/sw.js', { scope: '/build/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
---
+++ vite.config.ts
@@ -0,0 +1,57 @@
+import crypto from 'crypto';
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { VitePWA } from 'vite-plugin-pwa';
+
+let routesHash: string | undefined;
+const getOrBuildHash = () => {
+    if (!routesHash) {
+        const useRandom = `${Math.random()}-${new Date().toISOString()}`;
+        const cHash = crypto.createHash('MD5');
+        cHash.update(useRandom, 'utf-8');
+        routesHash = cHash.digest('hex');
+    }
+    return routesHash;
+};
+
+const cachePages = ['dashboard', 'agenda', 'assignments'];
+const additionalManifestEntries: any[] = [];
+
+cachePages.forEach((path: string) => {
+    additionalManifestEntries.push({
+        url: path.startsWith('/') ? path.substring(1) : path,
+        revision: `${getOrBuildHash()}`,
+    });
+});
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.js', 'resources/css/app.scss'],
+            refresh: true,
+        }),
+        VitePWA({
+            injectRegister: 'script',
+            strategies: 'injectManifest',
+            srcDir: 'resources/js',
+            outDir: 'public',
+            filename: 'sw.js',
+            scope: '/',
+            base: '',
+            manifest: {
+                name: 'Schedulist',
+                short_name: 'Schedulist',
+                start_url: '/app',
+                display: 'standalone',
+                theme_color: '#3367D6',
+                background_color: '#121212',
+                scope: '/',
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
+});
