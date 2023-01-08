ab4af0050b05fd76b9f2e583f41da543a18f1dba
Alex <alex.batts05@gmail.com>
2023-01-08 16:28:29 -0500

    Fix footer wrap

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,13 +1,13 @@
-<footer class="px-15 absolute clear-both mt-5 w-full border-t border-gray-200 py-10">
+<footer class="px-15 absolute clear-both w-full py-10 mt-5 border-t border-gray-200">
     <div class="flex flex-row flex-wrap content-start space-x-10 sm:space-x-16">
-        <div class="logo-image my-0 mt-2 ml-8 w-32 lg:ml-16"></div>
-        <div class="flex space-x-10 sm:space-x-16">
-            <a class="mdc-button text-primary mt-4 px-4 text-lg normal-case tracking-normal"
+        <div class="logo-image w-32 my-0 mt-2 ml-8 lg:ml-16"></div>
+        <div class="flex flex-wrap gap-x-10 sm:gap-x-16">
+            <a class="mdc-button text-primary px-4 mt-4 text-lg tracking-normal normal-case"
                 href="{{ route('contact') }}">
                 <span class="mdc-button__ripple"></span>
                 <span class="mdc-button__label">Contact Us</span>
             </a>
-            <a class="mdc-button text-primary mt-4 px-4 text-lg normal-case tracking-normal"
+            <a class="mdc-button text-primary px-4 mt-4 text-lg tracking-normal normal-case"
                 href="{{ route('privacy-policy') }}">
                 <span class="mdc-button__ripple"></span>
                 <span class="mdc-button__label">Privacy Policy</span>
