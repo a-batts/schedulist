622faf24ee6aaeb8a58d154435645d294fe950f2
Alex <alex.batts05@gmail.com>
2023-01-18 00:12:42 -0500

    Add location text to item

--- resources/views/components/agenda/item.blade.php
+++ resources/views/components/agenda/item.blade.php
@@ -4,7 +4,7 @@
     :class="`${'background-' + getItemColor(item.id, item.color)} ${'agenda-item-' + index  }`"
     :style="`top: ${item.top}px; width: calc(${item.width}% - .25rem ); left: calc(${item.left}% + .25rem); height: calc(${item.bottom}px - ${item.top}px);`">
     <div class="mdc-card__primary-action h-full pb-2" tabindex="0"
-        :class="`${ view == 'day' ? 'px-5' : 'px-3'} ${item.bottom - item.top > 80 && view == 'day' ? 'py-5' : 'py-1.5'}`">
+        :class="`${ view == 'day' ? 'px-5' : 'px-3'} ${item.bottom - item.top > 80 && view == 'day' ? 'py-4' : 'py-1.5'}`">
         <p class="agenda-text-primary font-medium"
             :class="{
                 'text-lg ': view == 'day',
@@ -12,6 +12,7 @@
                     'week'
             }"
             x-text="item.name"></p>
+        <p class="agenda-text-secondary text-sm" x-text="item.data?.['location'] ?? ''"></p>
         <p class="agenda-text-secondary text-sm transition-all" :class="{ 'hidden': view == 'week' }">
             <span x-text="item.startString"></span>
             <template x-if="item.endString != null">
