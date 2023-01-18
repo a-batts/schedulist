86d1ed2aedc586f4a42277cb96e424a4f9784a08
Alex <alex.batts05@gmail.com>
2023-01-17 22:23:44 -0500

    Fix agenda item styling

--- resources/views/components/agenda/item.blade.php
+++ resources/views/components/agenda/item.blade.php
@@ -4,7 +4,7 @@
     :class="`${'background-' + getItemColor(item.id, item.color)} ${'agenda-item-' + index  }`"
     :style="`top: ${item.top}px; width: calc(${item.width}% - .25rem ); left: calc(${item.left}% + .25rem); height: calc(${item.bottom}px - ${item.top}px);`">
     <div class="mdc-card__primary-action h-full pb-2" tabindex="0"
-        :class="`${ view == 'day' ? 'px-5' : 'px-3'} ${item.bottom - item.top > 100 && view == 'day' ? 'py-5' : 'py-1.5'}`">
+        :class="`${ view == 'day' ? 'px-5' : 'px-3'} ${item.bottom - item.top > 80 && view == 'day' ? 'py-5' : 'py-1.5'}`">
         <p class="agenda-text-primary font-medium"
             :class="{
                 'text-lg ': view == 'day',
